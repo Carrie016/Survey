05edb0df696211da6a9d083d66e4a0779d1402ff
carrie016 <wang.xin9@husky.neu.edu>
2020-07-19 03:30:38 -0700

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,9 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+
+  //123456Abc
+  //mongodb+srv://myuser:654Abc@cluster0.kc9md.mongodb.net/dbase?retryWrites=true&w=majority
+};
