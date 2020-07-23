13e5ae99e77ee6ca412487b953c37e019f797347
carrie016 <wang.xin9@husky.neu.edu>
2020-07-23 04:30:49 -0700

    2ed

--- config/prod.js
+++ config/prod.js
@@ -6,8 +6,8 @@ module.exports = {
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
   // stripePublishableKey:
   // "pk_test_51H7Xe3BegJpnimM4YmZ1zde8xgnOYvfWz6wcLYwVXxGp2u8wkVSjWnP7ZoX3D8eOGVdb3DgxlXzGNRtraISIEE1100XgqwfkVS",
-  // stripeSecretKey: process.env.STRIPE_SECRET_KEY,
-  stripeSecretKey: "112",
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+  // stripeSecretKey: "112",
 
   //123456Abc
   //mongodb+srv://myuser:654Abc@cluster0.kc9md.mongodb.net/dbase?retryWrites=true&w=majority
