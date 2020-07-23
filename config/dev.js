1497f8b1e336907c58f31b5e4051d4ebd9b0f17e
carrie016 <wang.xin9@husky.neu.edu>
2020-07-23 02:40:36 -0700

    2ed

---
+++ config/dev.js
@@ -0,0 +1,17 @@
+module.exports = {
+  googleClientID:
+    "652487753559-te2e075lfpdjneo09gvnchmll026cvhd.apps.googleusercontent.com",
+  googleClientSecret: "kNvo3gwgG5F0fkOoHEZ2-fEX",
+  mongoURI:
+    "mongodb+srv://carrie:123Abc@emaily.gvsdj.mongodb.net/mydb?retryWrites=true&w=majority",
+  cookieKey: "redacted",
+  stripePublishableKey:
+    "pk_test_51H7Xe3BegJpnimM4YmZ1zde8xgnOYvfWz6wcLYwVXxGp2u8wkVSjWnP7ZoX3D8eOGVdb3DgxlXzGNRtraISIEE1100XgqwfkVS",
+  stripeSecretKey:
+    "sk_test_51H7Xe3BegJpnimM4ARZJoHztrYOiNxvwwAVXwyHpcWZOkIt9BFg019gPACsXwC6RD7VckBaDHz10HQgi3CsD943B00m01mBgPp",
+  // one option to fix the https problem:
+  // googleRedirectURI: 'https://sleepy-tundra-26030.herokuapp.com'
+
+  //password somewhere: 123456Abc
+  //mongodb+srv://myuser:654Abc@cluster0.kc9md.mongodb.net/dbase?retryWrites=true&w=majority
+};
